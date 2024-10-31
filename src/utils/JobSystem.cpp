35a010a398c1e0217fd83bb416f22b9e73566f36
Marcus Gugacs <iimpaq@proton.me>
2024-10-31 12:25:50 +0100

    Added job system for easy multithreading

---
+++ src/utils/JobSystem.cpp
@@ -0,0 +1,86 @@
+/**
+* @file JobSystem.hpp
+* @author Marcus Gugacs
+* @date 10/31/2024
+* @copyright Copyright (c) 2024 Marcus Gugacs. All rights reserved.
+*/
+
+#include "JobSystem.hpp"
+
+#include "core/Assert.hpp"
+#include "os/ScopeLock.hpp"
+
+using namespace ntl;
+
+namespace atlas {
+  void JobSystem::Initialize() {
+    m_thread_count = std::thread::hardware_concurrency();
+    m_running_jobs = 0;
+    m_initialized = true;
+    initThreadPool();
+  }
+
+  void JobSystem::Shutdown() {
+    m_initialized = false;
+  }
+
+  void JobSystem::initThreadPool() {
+    VERIFY(m_initialized && "JobSystem must be initialized prior to use")
+
+    for(unsigned int i = 0; i < m_thread_count; ++i) {
+      std::thread worker{
+        [this]() {
+          while(m_initialized) {
+            auto job = JobSystem::Instance().GetJobOrWait();
+            job();
+            --m_running_jobs;
+          }
+        }
+      };
+      worker.detach();
+    }
+  }
+
+  void JobSystem::AddJob(const std::function<void()>& a_job) {
+    VERIFY(m_initialized && "JobSystem must be initialized prior to use")
+
+    ScopeLock lock(&m_jobs_lock);
+    m_jobs.Put(a_job);
+    m_jobs_changed.Broadcast();
+  }
+
+  std::function<void()> JobSystem::GetJobOrWait() {
+    VERIFY(m_initialized && "JobSystem must be initialized prior to use")
+
+    m_jobs_lock.Acquire();
+
+    while(m_jobs.IsEmpty())
+      m_jobs_changed.Wait();
+
+    ++m_running_jobs;
+    auto job = m_jobs.Get();
+
+    m_jobs_changed.Broadcast();
+    m_jobs_lock.Release();
+
+    return job;
+  }
+
+  void JobSystem::WaitForJobsToFinish() {
+    VERIFY(m_initialized && "JobSystem must be initialized prior to use")
+
+    m_jobs_lock.Acquire();
+
+    while(!m_jobs.IsEmpty() || (m_running_jobs > 0))
+      m_jobs_changed.Wait();
+
+    m_jobs_lock.Release();
+  }
+
+  Size JobSystem::GetPendingJobCount() {
+    m_jobs_lock.Acquire();
+    Size temp_count = m_jobs.GetSize();
+    m_jobs_lock.Release();
+    return temp_count;
+  }
+}
---
+++ src/utils/JobSystem.hpp
@@ -0,0 +1,85 @@
+/**
+* @file JobSystem.hpp
+* @author Marcus Gugacs
+* @date 10/31/2024
+* @copyright Copyright (c) 2024 Marcus Gugacs. All rights reserved.
+*/
+
+#ifndef NTL_JOB_SYSTEM_HPP
+#define NTL_JOB_SYSTEM_HPP
+
+#include <functional>
+#include <thread>
+
+#include "data/Array.hpp"
+#include "data/Bool.hpp"
+#include "data/Queue.hpp"
+#include "data/Singleton.hpp"
+#include "os/Lock.hpp"
+#include "os/Condition.hpp"
+
+namespace atlas {
+  /**
+   * @brief JobSystem class used to distribute jobs in a pool of threads.
+   */
+  class JobSystem : public ntl::Singleton<JobSystem> {
+    SINGLETON_IMPL(JobSystem)
+
+  private:
+    ntl::Size m_thread_count;
+    ntl::Array<std::thread> m_workers;
+    ntl::Queue<std::function<void()>> m_jobs;
+    ntl::Lock m_jobs_lock;
+    ntl::Condition m_jobs_changed;
+    std::atomic<ntl::Bool> m_initialized;
+    std::atomic<int> m_running_jobs;
+
+  public:
+    /**
+     * @brief Initializes the job system (singleton).
+     */
+    void Initialize();
+    /**
+     * @brief Shuts down the job system (singleton).
+     */
+    void Shutdown();
+
+    /**
+     * @brief Adds the given job to the job queue.
+     * @param a_job the job to add to the queu
+     */
+    void AddJob(const std::function<void()>& a_job);
+
+    /**
+     * @brief Gets the next job or waits until a job becomes available.
+     * @return the next job from the queue
+     */
+    std::function<void()> GetJobOrWait();
+    /**
+     * @brief Waits for all jobs to finish.
+     */
+    void WaitForJobsToFinish();
+
+    ntl::Size GetPendingJobCount();
+
+  private:
+    /**
+     * @brief Default Constructor.
+     */
+    JobSystem()
+      : Singleton{}, m_thread_count{0}, m_workers{}, m_jobs{}, m_jobs_lock{}, m_jobs_changed{&m_jobs_lock},
+        m_initialized{false} {}
+
+    /**
+     * @brief Default Destructor.
+     */
+    ~JobSystem() {}
+
+    /**
+     * @brief Initializes the thread pool.
+     */
+    void initThreadPool();
+  };
+}
+
+#endif // NTL_JOB_SYSTEM_HPP
